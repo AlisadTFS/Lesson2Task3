dfcdb7986de02d412f9b4ad16ac031e4a457af65
Alex <virdata@yandex.ru>
2022-03-04 01:29:17 +0800

    Выполнил задание 4

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -10,6 +10,22 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            string namemarket = "Зебра";
+            string addresmarket = "г. Воронеж, ул. Бульвара Победы, д. 256";
+            long inn = 7721511903;
+            DateTime datetimesale = DateTime.Now;
+            int codeproduct = 1158;
+            string nameproduct = "Мясо птицы";
+            float quantityproduct = 1.40f;
+            float priceproduct = 250;
+            float sumprice = quantityproduct * priceproduct;
+            Console.WriteLine($"{namemarket, 40}");
+            Console.WriteLine($"{addresmarket, 60}");
+            Console.WriteLine($"{inn,45}");
+            Console.WriteLine($"             Дата продажи: {datetimesale.ToString("d")} время продажи: {datetimesale.ToString("t")}");
+            Console.WriteLine("Код продукта      Найменование       Кол-во         Цена        Сумма");
+            Console.WriteLine($"{codeproduct} {nameproduct, 25} {quantityproduct.ToString("f2"), 10} кг. {priceproduct.ToString("f2"), 11}р. {sumprice.ToString("f2"), 9}р.");
+
         }
     }
 }
